a3cc6d62bf4108dad6b2ea941dc948d13ed755aa
Andrea <andrea.ciraolo@gmail.com>
2025-09-27 20:21:00 +0200

    feat: Add image optimization and personal photo
    - Integrate @11ty/eleventy-img for responsive image generation
    - Add optimizedImage shortcode with multiple formats (AVIF, WebP, JPEG)
    - Replace header placeholder with actual personal photo
    - Bump version to 1.2.0 for new image processing features
    - Configure automatic image optimization with lazy loading

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,36 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/css/");
-  eleventyConfig.addPassthroughCopy({ "src/assets/img": "assets/img" });
-  eleventyConfig.addPassthroughCopy({ "src/assets/js": "assets/js" });
+const Image = require('@11ty/eleventy-img');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./src/css/');
+  eleventyConfig.addPassthroughCopy({ 'src/js': 'assets/js' });
+
+  // Image optimization shortcode
+  eleventyConfig.addAsyncShortcode('optimizedImage', async function(src, alt, sizes = '100vw', className = '') {
+    const metadata = await Image(src, {
+      widths: [320, 640, 960, 1200],
+      formats: ['avif', 'webp', 'jpeg'],
+      outputDir: './site/assets/img/',
+      urlPath: '/assets/img/'
+    });
+
+    const imageAttributes = {
+      alt,
+      sizes,
+      loading: 'lazy',
+      decoding: 'async',
+      class: className
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+  });
 
   return {
     dir: {
-      input: "pages",
-      output: "site",
-      includes: "../src/includes",
-      layouts: "../src/layouts",
-      data: "../src/data"
-    }
+      input: 'pages',
+      output: 'site',
+      includes: '../src/includes',
+      layouts: '../src/layouts',
+      data: '../src/data',
+    },
   };
 };
